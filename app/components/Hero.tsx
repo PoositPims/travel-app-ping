ed0e3ce47b2346dd02546a9587952da38c757fa3
Poosit <poosit.pims@gmail.com>
2024-07-21 22:56:28 +0700

    finish camp component

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -5,7 +5,6 @@ function Hero() {
   return (
     <section className="max-container padding-container flex flex-col gap-20 py-10 pb-32 md:gap-28 lg:py-20 xl:flex-row border-2 ">
       <div className="hero-map" />
-      {/* left side */}
       <div className="relative z-20 flex flex-1 flex-col xl:w-1/2">
         <Image
           src="/camp.svg"
@@ -52,7 +51,6 @@ function Hero() {
         </div>
       </div>
 
-      {/* Right */}
       <div className="relative flex flex-1 items-start">
         <div className="relative z-20 flex w-[268px] flex-col gap-8 rounded-3xl bg-green-90 px-7 py-8">
           <div className="flex flex-col">
