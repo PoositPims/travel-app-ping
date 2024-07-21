50627c426f8eefd6f55f832b09c4307420e75841
Poosit <poosit.pims@gmail.com>
2024-07-21 16:09:56 +0700

    first commit with navbar

---
+++ app/components/Hero.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Hero() {
+  return (
+    <section className="max-container padding-container flex flex-col gap-20 py-10 pb-32 md:gap-28 lg:py-20 xl:flex-row border-2 border-red-600">
+      <div className="hero-map" />
+      {/* left side */}
+      <div></div>
+    </section>
+  );
+}
+
+export default Hero;
